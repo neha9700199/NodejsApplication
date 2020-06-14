a7d32b8f98ceb7f5263b57011086742c85eb8a0f
root <root@loadbalancer>
2020-06-14 11:53:10 +0000

    now i am quite clear

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-//Launch listening server on port 8080
-app.listen(8080, function () {
-  console.log('App listening on port 8080!')
+//Launch listening server on port 8000
+app.listen(8000, function () {
+  console.log('App listening on port 8000!')
 })
