7efbbbf5ae1eacfb22a52ed7b368bac6a015e981
root <root@loadbalancer>
2020-06-18 20:36:18 +0000

    this is app json

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World to the entire world at one go')
+  res.send('Hello World')
 })
 
 //Launch listening server on port 8000
