63f2ce383de457a27696078728fde7cf94bb8afd
root <root@loadbalancer>
2020-06-19 18:23:59 +0000

    i am a new branch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hi! i am Nodejs-branch2')
 })
 
 //Launch listening server on port 8000
